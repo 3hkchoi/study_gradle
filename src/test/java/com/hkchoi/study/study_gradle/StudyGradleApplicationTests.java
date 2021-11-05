17758a561ed859828a5b3ea09c061ecf7e9448a2
3hkchoi <3hkchoi@gmail.com>
2021-11-06 00:36:14 +0900

    first gradelStudy Project

---
+++ src/test/java/com/hkchoi/study/study_gradle/StudyGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkchoi.study.study_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
